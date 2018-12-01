362c8f89e8d422161c1e6ae011ad87406ef1a797
Michael Thiem <m.thiem@innoxchain-dev>
2018-12-01 12:36:18 -0800

    added build folder

--- cmd/ixclient/ixclient.go
+++ cmd/ixclient/ixclient.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/innoxchain/ixstorage/pkg/apps/ixclient"
+	"github.com/innoxchain/ixstorage/build"
 )
 
 func main() {
 	fmt.Println(ixclient.Greet("world"))
+	fmt.Println("Version: ", version.Version)
 }
