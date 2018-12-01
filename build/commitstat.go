9d68a8d7069a7235453dbd5f0f87c699750e61b6
Michael Thiem <m.thiem@innoxchain-dev>
2018-12-01 12:53:30 -0800

    added commit status

---
+++ build/commitstat.go
@@ -0,0 +1,12 @@
+package build
+
+var (
+	//Commit is the commit hash from git
+	Commit string
+
+	//Branch is the git branch name of this build
+	Branch string
+
+	//BuildTime is the time when this built has been completed
+	BuildTime string
+)
