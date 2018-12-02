5086f753d48d1765dd299b7796da63d62da4f4f5
Michael Thiem <m.thiem@innoxchain.com>
2018-12-02 13:01:47 -0800

    added first draft of cobra cli support

--- cmd/ixclient/go.mod
+++ cmd/ixclient/go.mod
@@ -1,6 +1,8 @@
 module github.com/innoxchain/ixstorage/cmd/ixclient
 
 require (
-	github.com/innoxchain/ixstorage/build v0.0.2
+	github.com/innoxchain/ixstorage/build v0.0.3
 	github.com/innoxchain/ixstorage/pkg/apps/ixclient v0.0.1
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 )
--- cmd/ixclient/ixclient.go
+++ cmd/ixclient/ixclient.go
@@ -2,13 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/innoxchain/ixstorage/pkg/apps/ixclient"
 	"github.com/innoxchain/ixstorage/build"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	fmt.Println(ixclient.Greet("world"))
-	fmt.Println("Commit: ", build.Commit)
-	fmt.Println("Version: ", build.Version)
+
+	ixclient.Greet("world")
+
+	root := &cobra.Command{
+		Use:   "ixclient cli",
+		Short: "ixclient cli",
+		Long:  "ixclient command line interface",
+		Run: defaultStatus,
+	}
+
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Show detailed version information",
+		Long:  "Show detailed information.",
+		Run:   showVersion,
+	}
+
+	root.AddCommand(versionCmd)
+
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
+
+func defaultStatus(cmd *cobra.Command, args []string) {
+	fmt.Println("Run xclient version for detailed version information")
+}
+
+func showVersion(cmd *cobra.Command, args []string) {
+	fmt.Println("ixclient")
+	fmt.Println("\tVersion " + build.Version)
+	fmt.Println("\tCommit " + build.Commit)
+	fmt.Println("\tBranch " + build.Branch)
+	fmt.Println("\tBuild Time " + build.BuildTime)
+} 
